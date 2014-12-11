c174fdfd691e612ea07c5477b4fff028e18e2f70
boxmein <wsdf294@gmail.com>
2014-12-12 00:10:21 +0200

    commented the routes to describe what they are

--- src/main.js
+++ src/main.js
@@ -3,19 +3,25 @@ var app = angular.module('fullmoviefinder', ['ui.router']);
 // Configure routing
 app.config(['$urlRouterProvider', '$stateProvider', 
             function($urlRouterProvider, $stateProvider) {
+  
+  // Index page - include whatever you want
   $stateProvider.state('home', {
     url: '/',
     templateUrl: 'partials/home.partial.html'
   })
+  // Results page, also directly linkable
   .state('results', {
     url: '/search/:movie',
     templateUrl: 'partials/search-results.partial.html'
   })
+  // Video watch page, also directly linkable
   .state('video', {
     url: '/video/:link',
-    templateUrl: 'partials/video-view.partial.html'
+    controller: 'VideoCtrl',
+    templateUrl: 'partials/video-view.partial.html',
   });
 
+  // Default to the index page.
   $urlRouterProvider.otherwise('/');
 }]);
 
