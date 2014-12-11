f9a65bc6cd24c7f90a3fe8d2545c241f7b38d588
boxmein <wsdf294@gmail.com>
2014-12-12 00:11:24 +0200

    finish SearchCtrl, also include starter VideoCtrl

--- src/controllers.js
+++ src/controllers.js
@@ -1,8 +1,31 @@
 
 
 // Controller for the search box
-angular.module('fullmoviefinder')
-  .controller('SearchCtrl', ['$scope','redditSearch','$stateParams',
+app.controller('SearchCtrl', ['$scope','redditSearch','$stateParams',
                            function($scope, redditSearch, $stateParams) {
-  $scope.results = redditSearch.resultsForMovie($stateParams.movie);
+
+  $scope.update = function(movie) {
+    console.log('[SearchCtrl#update] calling search update');
+    var promise = redditSearch.resultsForMovie(movie);
+    promise.then(function(results) {
+    
+      $scope.results = _.flatten(results);
+
+      console.log('[SearchCtrl#update] results are in: ', $scope.results);
+    });
+
+  };
+
+  if ($stateParams.movie)
+    $scope.update($stateParams.movie);
+
+}]);
+
+app.controller('VideoCtrl', ['$scope', '$stateParams', 
+                             function($scope, $stateParams) {
+  // you can either link to this via stateParams
+  if (!$stateParams.notFromLink)
+    return $scope.link = $stateParams.link;
+
+  // or call it via Javascript $state.go('video')
 }]);
