da8bdd27caa89c3ec62028dc7ac5fc4df54db81c
boxmein <wsdf294@gmail.com>
2014-12-12 00:10:56 +0200

    <search-box> uses SearchCtrl, polish out <reddit-post> with underscore

--- src/directives.js
+++ src/directives.js
@@ -1,21 +1,14 @@
 
 var app = angular.module('fullmoviefinder');
 
-// Custom HTML tag <reddit-post title="" href=""></reddit-post>
+// Custom HTML tag <reddit-post title="" url=""></reddit-post>
 app.directive('redditPost', [ function() {
   return {
     restrict: 'E', // A = HTML attribute, E = HTML element, C = class name
     templateUrl: 'partials/reddit-post.partial.html',
-
-    // Clear out the scope (otherwise, includes everything above the element)
-    scope: {},
     
     link: function(scope, el, attr) {
-      // Add more attributes here if you want to
-      // scope = {{variables}} that the HTML can use
-      // attr = HTML attributes on the custom tag
-      scope.href = attr.href || '';
-      scope.title = attr.title || '';
+      _.extend(scope, attr);
     }
   };
 }]);
@@ -23,6 +16,7 @@ app.directive('redditPost', [ function() {
 app.directive('searchBox', [ function() {
   return {
     restrict: 'E',
+    controller: 'SearchCtrl',
     templateUrl: 'partials/search-box.partial.html'
   };
 }]);
